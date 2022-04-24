7e7e48c0ced0d9ed5bfaa2f4bb81503b008e6de4
Benjamin <carlosbpac@gmail.com>
2022-04-24 12:10:11 -0600

    organizacion de codigo

--- CRL-EDITOR/src/app/contenedor-editor/contenedor-editor.component.ts
+++ CRL-EDITOR/src/app/contenedor-editor/contenedor-editor.component.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
-import {FormControl} from '@angular/forms';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 /**
  * @title Tab group with dynamically changing tabs
@@ -9,11 +10,22 @@ import {FormControl} from '@angular/forms';
   templateUrl: './contenedor-editor.component.html',
   styleUrls: ['./contenedor-editor.component.css']
 })
-export class ContenedorEditorComponent{
-  tabs = ['First', 'Second', 'Third'];
+export class ContenedorEditorComponent {
+
+  tabs = ['Tab 1', 'Tab 2', 'Tab 3'];
   selected = new FormControl(0);
+  tabtitle: string = '';
+
   addTab(selectAfterAdding: boolean) {
-    this.tabs.push('New');
+
+    if (this.tabtitle != '') {
+      this.tabs.push(this.tabtitle);
+    } else {
+      this.tabs.push('New');
+    }
+
+    this.tabtitle = '';
+    
     if (selectAfterAdding) {
       this.selected.setValue(this.tabs.length - 1);
     }
@@ -21,4 +33,5 @@ export class ContenedorEditorComponent{
   removeTab(index: number) {
     this.tabs.splice(index, 1);
   }
+
 }
