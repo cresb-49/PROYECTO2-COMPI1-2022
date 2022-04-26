d395e54e15cbd1acbb5ea24a8bc7a19ad69e0a4e
benjamin <carlosbpac@gmail.com>
2022-04-25 20:36:49 -0600

    componente dinamico consola, editor

--- CRL-EDITOR/src/app/contenedor-principal/contenedor-principal.component.ts
+++ CRL-EDITOR/src/app/contenedor-principal/contenedor-principal.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { ConsolaCRLComponent } from '../consola-crl/consola-crl.component';
+import { ContenedorEditorComponent } from '../contenedor-editor/contenedor-editor.component';
+import { DynamicComponentDirective } from '../directives/dynamic-component.directive';
 
 @Component({
   selector: 'app-contenedor-principal',
   templateUrl: './contenedor-principal.component.html',
   styleUrls: ['./contenedor-principal.component.css']
 })
-export class ContenedorPrincipalComponent implements OnInit {
+export class ContenedorPrincipalComponent implements AfterViewInit {
+  contenedorPrincipal:ContenedorEditorComponent
+  consolaCRL:ConsolaCRLComponent
 
-  constructor() { }
-
-  ngOnInit(): void {
+  @ViewChild(DynamicComponentDirective) dynamic: DynamicComponentDirective;
+  
+  ngAfterViewInit(): void {
+    const viewContainerRef = this.dynamic.viewContainerRef;
+    const componentRef1 = viewContainerRef.createComponent<ContenedorEditorComponent>(ContenedorEditorComponent);
+    this.contenedorPrincipal=componentRef1.instance;
+    const componentRef2 = viewContainerRef.createComponent<ConsolaCRLComponent>(ConsolaCRLComponent);
+    this.consolaCRL = componentRef2.instance;
   }
 
   obtenerCodigo(){
