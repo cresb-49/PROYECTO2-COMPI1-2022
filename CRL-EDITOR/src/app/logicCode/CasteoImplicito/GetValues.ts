0f000358131b5f3eecb95effe2b30785f89cb117
Benjamin <carlosbpac@gmail.com>
2022-04-27 13:09:58 -0600

    implementacion del analizador en TS

--- CRL-EDITOR/src/app/logicCode/CasteoImplicito/GetValues.ts
+++ CRL-EDITOR/src/app/logicCode/CasteoImplicito/GetValues.ts
@@ -12,7 +12,17 @@ export class GetValues{
     }
 
     public getCharNumeric(caracter:String){
-        
+        return caracter.charCodeAt(0);
+    }
+
+    public getCharAtString(caracter:string){
+        let legth = caracter.length;
+        return caracter.substring(1,(legth-2));
+    }
+
+    public getSubString(cadena:string){
+        let length = cadena.length;
+        return cadena.substring(1,(length-2));
     }
 
 }
