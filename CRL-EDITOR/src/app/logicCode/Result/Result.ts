2c1dc9d9136f4e73cacea762742f6ed293c7033f
benjamin <carlosbpac@gmail.com>
2022-05-02 13:16:12 -0600

    actualizacion del result del analisis para funcon principal y imports

--- CRL-EDITOR/src/app/logicCode/Result/Result.ts
+++ CRL-EDITOR/src/app/logicCode/Result/Result.ts
@@ -1,8 +1,10 @@
 import { Declaracion } from "../Instrucciones/Declaracion";
+import { Importar } from "../Instrucciones/Importar";
 import { Mostrar } from "../Instrucciones/Mostrar";
+import { Principal } from "../Instrucciones/Principal";
 import { Sentencias } from "../Instrucciones/Sentencias";
 
 export class Result {
     constructor(public instrucciones:any[],public errores:any[],public sentencias:Array<Sentencias>,
-        public mostra:Array<Mostrar>,public varaiblesGlobales:Array<Declaracion>) {}
+        public mostra:Array<Mostrar>,public varaiblesGlobales:Array<Declaracion>,public imports:Array<Importar>,public principal:Principal|null) {}
 }
