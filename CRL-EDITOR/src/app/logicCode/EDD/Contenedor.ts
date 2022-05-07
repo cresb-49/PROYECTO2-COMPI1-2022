f70054dedc9f1ec29f5ea3dc0fdae6a2195ea1fb
benjamin <carlosbpac@gmail.com>
2022-05-06 20:50:45 -0600

    EDD de datos contenedor

---
+++ CRL-EDITOR/src/app/logicCode/EDD/Contenedor.ts
@@ -0,0 +1,20 @@
+export class Contenedor<T> {
+    private drive:Map<string,Map<string,T>>;
+    constructor() {
+        this.drive = new Map();
+    }
+    public has(key1:string,key2:string){
+        if(this.drive.has(key1)){
+            if(this.drive.get(key1)?.has(key2)){
+                return true;
+            }
+        }
+        return false;
+    }
+    public get(key1:string,key2:string){
+        return this.drive.get(key1)?.get(key2);
+    }
+    public set(key1:string,key2:string,value:T){
+        this.drive.get(key1)?.set(key2,value);
+    }
+}
