d2d9bd5aa0ce82953e08a597866aa67f16ab0422
Benjamin <carlosbpac@gmail.com>
2022-05-14 22:33:42 -0600

    descarga de los archivos svg del renderizado de imagen

--- CRL-EDITOR/src/app/grafico/grafico.component.ts
+++ CRL-EDITOR/src/app/grafico/grafico.component.ts
@@ -22,4 +22,18 @@ export class GraficoComponent implements OnInit {
   d3(){
     graphviz('#'+this.id).renderDot(this.grap);
   }
+
+  download(){
+    let obj:any=document.getElementById(this.id);
+    let svgBody:string = obj.innerHTML;
+    if(svgBody.length!=0){
+      //let svgContent="<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" viewBox=\"4239.54 906.94 3137 1984\">"+svgBody+"</svg>";
+      var blob = new Blob([svgBody], { type: 'image/svg+xml'});
+      var url = window.URL.createObjectURL(blob);
+      var anchor = document.createElement("a");
+      anchor.download = ("Grafico#"+this.id);
+      anchor.href = url;
+      anchor.click();
+    }
+  }
 }
